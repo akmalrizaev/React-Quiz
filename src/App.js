8b2cd59d7c9f1906c5e6c8327a8a895ee3d0c85a
Akmal Rizayev <akmal.rizayev@gmail.com>
2023-07-14 12:56:03 +0500

    The React Quiz App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import DateCounter from './DateCounter';
+import Header from './Header';
+import Main from './Main';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
