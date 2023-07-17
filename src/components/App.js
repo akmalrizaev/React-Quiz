f55834d07df949e1d48627841ae20ae4e53c5ed0
Akmal Rizayev <akmal.rizayev@gmail.com>
2023-07-17 21:13:12 +0500

    Restarting a Quiz

--- src/components/App.js
+++ src/components/App.js
@@ -55,6 +55,8 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case 'restart':
+      return { ...initialState, questions: state.questions, status: 'ready' };
 
     default:
       throw new Error('Action unknown');
@@ -114,6 +116,7 @@ function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
           // <p>You Finished</p>
         )}
